4fc3c429bfd58e337a7283e4a2db81bdf0356a65
Asish <apatro817@gmail.com>
2024-10-04 01:34:11 +0530

    registration edit

--- src/main/java/com/smartbiz/repository/UserRepository.java
+++ src/main/java/com/smartbiz/repository/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.smartbiz.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, Long>{
 	Optional<User> findByEmail(String email);
 	
 
